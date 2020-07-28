c95aa26f4d8788804e0a59a31236359e93295f10
sharma2910 <darshit.sharma29@gmail.com>
2020-07-28 16:22:41 +0530

    CRUD Operations with Auth

---
+++ src/tasks/task.repository.ts
@@ -0,0 +1,44 @@
+import { Task } from './task.entity';
+import { EntityRepository, Repository } from 'typeorm';
+import { CreateTaskDto } from './dto/create-task.dto';
+import { TaskStatus } from './task-status.enum';
+import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
+import { User } from 'src/auth/user.entity';
+
+@EntityRepository(Task)
+export class TaskRepository extends Repository<Task> {
+
+    async getTasks(filterDto: GetTasksFilterDto, user: User): Promise<Task[]> {
+        const { status, search } = filterDto;
+        const query = this.createQueryBuilder('task');
+
+        query.where('task.userId = :userId', { userId: user.id })
+
+        if (status) {
+            query.andWhere('task.status = :status', { status })
+        }
+
+        if (search) {
+            query.andWhere('task.title LIKE :search OR task.description LIKE :search', { search: `%${search}%` })
+        }
+
+        const tasks = await query.getMany();
+        return tasks;
+    }
+
+    async createTask(createTaskDto: CreateTaskDto, user: User): Promise<Task> {
+        const task = new Task();
+        const { title, description } = createTaskDto;
+
+        task.title = title;
+        task.description = description;
+        task.status = TaskStatus.OPEN;
+        task.user = user;
+        task.userId = user.id;
+
+        delete task.user;
+        await task.save();
+        return task;
+    }
+
+}
